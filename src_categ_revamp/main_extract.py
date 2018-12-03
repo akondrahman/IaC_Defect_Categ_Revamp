b94a843c6857fdf66594c8beabab93f8c9e1246b
Akond Rahman <akond.rahman.buet@gmail.com>
2018-12-02 20:37:57 -0500

    extraction

--- src_categ_revamp/main_extract.py
+++ src_categ_revamp/main_extract.py
@@ -7,6 +7,10 @@ import os
 import csv 
 import numpy as np
 import sys
+from git import Repo
+import  subprocess
+import time 
+import  datetime 
 
 def getEligibleProjects(fileNameParam):
   repo_list = []
@@ -32,6 +36,30 @@ def getRelPathOfFiles(all_pp_param, repo_dir_absolute_path):
   files_relative_paths = [os.path.relpath(path, common_path) for path in all_pp_param]
   return files_relative_paths 
 
+def getPuppRelatedCommits(repo_dir_absolute_path, ppListinRepo, branchName='master'):
+  mappedPuppetList=[]
+  track_exec_cnt = 0
+  repo_  = Repo(repo_dir_absolute_path)
+  all_commits = list(repo_.iter_commits(branchName))
+  for each_commit in all_commits:
+    track_exec_cnt = track_exec_cnt + 1
+
+    cmd_of_interrest1 = "cd  " + repo_dir_absolute_path + " ; "
+    cmd_of_interrest2 = "git show --name-status " + str(each_commit)  +  "  | awk '/.pp/ {print $2}'"
+    cmd_of_interrest = cmd_of_interrest1 + cmd_of_interrest2
+    commit_of_interest  = subprocess.check_output(['bash','-c', cmd_of_interrest])
+
+    for ppFile in ppListinRepo:
+      if ppFile in commit_of_interest:
+
+       file_with_path = os.path.join(repo_dir_absolute_path, ppFile)
+       mapped_tuple = (file_with_path, each_commit)
+       mappedPuppetList.append(mapped_tuple)
+
+  return mappedPuppetList
+
+
+
 def constructDataset(orgParamName, repo_name_param, branchParam):
 
   repo_path   = "/Users/akond/PUPP_REPOS/" + orgParamName + "/" + repo_name_param
@@ -43,7 +71,7 @@ def constructDataset(orgParamName, repo_name_param, branchParam):
   rel_path_pp_files = getRelPathOfFiles(all_pp_files_in_repo, repo_path)
   # print rel_path_pp_files
 
-  pupp_commits_in_repo = bug_git_util.getPuppRelatedCommits(repo_path, rel_path_pp_files, repo_branch)
+  pupp_commits_in_repo = getPuppRelatedCommits(repo_path, rel_path_pp_files, repo_branch)
   print pupp_commits_in_repo
 
   # all_pupp_msgs = bug_git_util.getPuppMessages(yes_bug_mapping, no_bug_mapping)
