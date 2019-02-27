7ddeb8aae44d5e5fb7dab3862a913980df5d4d7f
Akond Rahman <akond.rahman.buet@gmail.com>
2019-02-27 11:56:16 -0500

    only defect dump: openstack puppet

--- src_categ_revamp/defectDiffDump.py
+++ src_categ_revamp/defectDiffDump.py
@@ -4,6 +4,10 @@ Fed 27, 2019
 Get diffs for defect-related commits 
 '''
 import pandas as pd 
+import numpy as np 
+import csv 
+import os 
+import subprocess
 
 def getDiffStr(repo_path_p, commit_hash_p, file_p):
    
@@ -16,14 +20,11 @@ def getDiffStr(repo_path_p, commit_hash_p, file_p):
 
    return diff_output
 
-def dumpContentIntoFile(strP, fileP):
-  fileToWrite = open( fileP, 'w')
-  fileToWrite.write(strP )
-  fileToWrite.close()
-  return str(os.stat(fileP).st_size)
+
 
 def dumpDefectDiffText(df_p, fil_p):
     dump_str = ''
+    cnt  = 0 
     commit_hash_list = np.unique( df_p['COMMIT'].tolist() )
     for commit_hash in commit_hash_list:
         commit_df        = df_p[df_p['COMMIT']==commit_hash]
@@ -33,18 +34,18 @@ def dumpDefectDiffText(df_p, fil_p):
         date_time        = commit_df['TIME'].tolist()[0]
         for file_ in commit_pp_files: 
             diff_content_str = getDiffStr(commit_repo_path, commit_hash, file_)
-            print '='*25 + ':'*3  + repo_path  + ':'*3 + commit_hash + ':'*3 + 'START!' + '='*25
-            print file_ 
-            print '*'*10
-            print diff_
-            print '*'*10
-            print text_comm
-            print '*'*10
-            print 'DECISION===>:'
-            print '*'*10
-            print '='*25 + ':'*3  + date_time + ':'*3 + 'END!!!' + '='*25    
-            dump_str = dump_str + repo_path + ','  + commit_hash + ',' + file_ + ',' + date_time + ',' + text_comm + '\n'      
-    dumpContentIntoFile(dump_str, fil_p)        
+            if(len(diff_content_str) > 0 ):
+                cnt += 1 
+                print '='*25 + ':'*3  + commit_repo_path  + ':'*3 + commit_hash + ':'*3 + 'START!' + '='*25
+                print file_ 
+                print '*'*10
+                print diff_content_str
+                print '*'*10
+                print text_comm
+                print '*'*10
+                print 'DECISION===>:'
+                print '*'*10
+                print '='*25 + ':'*3 + str(cnt) + ':'*3  + date_time + ':'*3 + 'END!!!' + '='*25    
 
 
 if __name__=='__main__':
