d776dfdd9699aa21f95bc24e37d7ccac5b4d7244
Akond Rahman <akond.rahman.buet@gmail.com>
2019-03-20 11:48:41 -0400

    ACID: keyword matching stage for classifier

--- src_categ_revamp/ACID/classifier.py
+++ src_categ_revamp/ACID/classifier.py
@@ -24,4 +24,22 @@ def detectCateg(msg_, diff_):
         elif(any(x_ in msg_ for x_ in constants.doc_defect_kw_list )): 
             defect_categ = constants.DOC_DEFECT_CATEG
         elif(any(x_ in msg_ for x_ in constants.idem_defect_kw_list )): 
-            defect_categ = constants.IDEM_DEFECT_CATEG             
+            defect_categ = constants.IDEM_DEFECT_CATEG            
+        elif(any(x_ in msg_ for x_ in constants.logic_defect_kw_list )): 
+            defect_categ = constants.LOGIC_DEFECT_CATEG                    
+        elif(any(x_ in msg_ for x_ in constants.secu_defect_kw_list )): 
+            defect_categ = constants.SECU_DEFECT_CATEG  
+        elif(any(x_ in msg_ for x_ in constants.build_defect_kw_list )): 
+            defect_categ = constants.BLD_DEFECT_CATEG   
+        elif(any(x_ in msg_ for x_ in constants.db_defect_kw_list )): 
+            defect_categ = constants.DB_DEFECT_CATEG                                                           
+        elif(any(x_ in msg_ for x_ in constants.insta_defect_kw_list )): 
+            defect_categ = constants.INSTALL_DEFECT_CATEG                               
+        elif(any(x_ in msg_ for x_ in constants.logging_defect_kw_list )): 
+            defect_categ = constants.LOGGING_DEFECT_CATEG   
+        elif(any(x_ in msg_ for x_ in constants.network_defect_kw_list )): 
+            defect_categ = constants.NETWORK_DEFECT_CATEG                             
+        elif(any(x_ in msg_ for x_ in constants.race_defect_kw_list )): 
+            defect_categ = constants.RACE_DEFECT_CATEG 
+        elif(any(x_ in msg_ for x_ in constants.service_defect_kw_list )): 
+            defect_categ = constants.SERVICE_DEFECT_CATEG   
--- src_categ_revamp/ACID/constants.py
+++ src_categ_revamp/ACID/constants.py
@@ -25,16 +25,34 @@ FILE_WRITE_MODE = 'w'
 
 NO_DEFECT_CATEG = 'NO_DEFECT'
 CONFIG_DEFECT_CATEG = 'CONFIG_DEFECT'
-DEP_DEFECT_CATEG = 'DEP_DEFECT'
-DOC_DEFECT_CATEG = 'DOC_DEFECT'
+DEP_DEFECT_CATEG  = 'DEP_DEFECT'
+DOC_DEFECT_CATEG  = 'DOC_DEFECT'
 IDEM_DEFECT_CATEG = 'IDEM_DEFECT'
+LOGIC_DEFECT_CATEG = 'LOGIC_DEFECT'
+SECU_DEFECT_CATEG = 'SECU_DEFECT'
+BLD_DEFECT_CATEG  = 'BUILD_DEFECT'
+DB_DEFECT_CATEG   = 'DB_DEFECT' 
+INSTALL_DEFECT_CATEG   = 'INSTALL_DEFECT' 
+LOGGING_DEFECT_CATEG   = 'LOG_DEFECT'
+NETWORK_DEFECT_CATEG   = 'NET_DEFECT'
+RACE_DEFECT_CATEG      = 'RACE_DEFECT'
+SERVICE_DEFECT_CATEG   = 'SERVICE_DEFECT'
 
 prem_bug_kw_list      = ['error', 'bug', 'fix', 'issue', 'mistake', 'incorrect', 'fault', 'defect', 'flaw' ]
 config_defect_kw_list = ['connection', 'string', 'parameter', 'hash', 'value', 'configure', 'field', 'option', 'version', 'URL', 'setting', 'ip', 'repo', 'link', 'time', 'server', 'command', 'setting', 'hiera', 'data', 'sql', 'permissions', 'mode', 'dir', 'protocol', 'missing', 'reference', 'path', 'location', 'driver', 'port', 'protocol', 'gateway', 'tcp', 'udp', 'fact']
 dep_defect_kw_list    = ['dependency', 'relation', 'sync', 'compatibility', 'ordering', 'missing', 'ensure', 'package', 'conflict', 'name', 'inherit', 'module', 'merge', 'namespace', 'test']
 doc_defect_kw_list    = ['doc', 'comment', 'spec', 'license', 'copyright', 'notice'] 
 idem_defect_kw_list   = ['idempoten']
+logic_defect_kw_list  = ['logic', 'way', 'conditional', 'boolean']
+secu_defect_kw_list   = ['vulnerability', 'ssl', 'firewall', 'secret', 'authenticate', 'tls', 'ca_file', 'password', 'security']
 
+build_defect_kw_list  = ['build']
+db_defect_kw_list     = ['db', 'database']
+insta_defect_kw_list  = ['install']
+logging_defect_kw_list= ['log']
+network_defect_kw_list= ['provision', 'network', 'l23', 'balancer', 'domain', 'route', 'proxy', 'dhcp']
+race_defect_kw_list   = ['race']
+service_defect_kw_list= ['service', 'caching', 'backend', 'job', 'start', 'gate', 'stage', 'env', 'requirement', 'restore', 'server']
 
 DFLT_KW  = 'default'
 CLOSE_KW = 'closes-bug'
