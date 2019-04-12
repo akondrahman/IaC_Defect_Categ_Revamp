27760ab326db579845f702447745591d8c62f509
Akond Rahman <akond.rahman.buet@gmail.com>
2019-04-12 14:21:36 -0400

    metric mining work: loc added, deleted in diff

--- src_categ_revamp/metric-mining/metric_miner.py
+++ src_categ_revamp/metric-mining/metric_miner.py
@@ -64,13 +64,48 @@ def getDiffStr(repo_path_p, commit_hash_p, file_p):
 def getDiffLOC(diff_text):
     add_cnt, del_cnt = 0, 0 
     diff_text_list = diff_text.split('\n') 
-    diff_text_list = [x_ for x_ in diff_text_list if (('---' not in x_) or ('+++' not in x_)) ]
+    diff_text_list = [x_ for x_ in diff_text_list if (('---' not in x_) and ('+++' not in x_)) ]
     add_text_list  = [x_ for x_ in diff_text_list if x_.startswith('+')]
     del_text_list  = [x_ for x_ in diff_text_list if x_.startswith('-')]
 
-    print add_text_list, del_text_list 
+    # print add_text_list, del_text_list 
     add_cnt, del_cnt = len(add_text_list), len(del_text_list)
     return add_cnt, del_cnt 
+
+def getDevCountForFile(param_file_path, repo_path):
+   author_count      = 1 
+
+   cdCommand         = "cd " + repo_path + " ; "
+   theFile           = os.path.relpath(param_file_path, repo_path)
+   commitCountCmd    = " git blame "+ theFile +"  | awk '{print $2}' | cut -d'(' -f2 "
+   command2Run = cdCommand + commitCountCmd
+
+   commit_count_output = subprocess.check_output(['bash','-c', command2Run])
+   author_count_output = commit_count_output.split('\n')
+   author_count_output = [x_ for x_ in author_count_output if x_!='']
+   author_count        = len(np.unique(author_count_output))
+
+   return author_count
+
+def getDevsOfRepo(repo_path_param):
+   author_dict       = {}
+
+   cdCommand         = "cd " + repo_path_param + " ; "
+   commitCountCmd    = " git log --pretty=format:'%H_%an' "
+   command2Run = cdCommand + commitCountCmd
+
+   commit_count_output = subprocess.check_output(['bash','-c', command2Run])
+   author_count_output = commit_count_output.split('\n')
+   for commit_auth in author_count_output:
+       commit_ = commit_auth.split('_')[0]
+       
+       author_ = commit_auth.split('_')[1]
+       author_ = author_.replace(' ', '')
+
+       if commit_ not in author_dict:
+           author_dict[commit_] = author_ 
+
+   return author_dict  
 
 
 def mineCommitsOfTheRepo(repo_path_param, repo_branch_param, pupp_commits_mapping):
@@ -87,11 +122,17 @@ def mineCommitsOfTheRepo(repo_path_param, repo_branch_param, pupp_commits_mappin
 
         diff_content_str = getDiffStr(repo_path_param, commit_hash, file_)
         loc_add, loc_del = getDiffLOC(diff_content_str) 
+        loc_tot          = loc_add + loc_del 
 
+        dir_             = os.path.dirname(file_) 
+        devs_for_file    = getDevCountForFile(file_, repo_path_param) 
+
+        metric_tuple = (file_, dir_, repo_path_param, loc_tot, devs_for_file)  
+        print metric_tuple
         if commit_hash not in files_changed_dict:
-           files_changed_dict[commit_hash] = [(file_, repo_path_param)] 
+           files_changed_dict[commit_hash] = [metric_tuple] 
         else: 
-           files_changed_dict[commit_hash] = files_changed_dict[commit_hash] + [(file_, repo_path_param)]  
+           files_changed_dict[commit_hash] = files_changed_dict[commit_hash] + [metric_tuple]  
 
 
 
@@ -99,6 +140,9 @@ def runMiner(orgParamName, repo_name_param, branchParam):
   
   repo_path   = '/Users/akond/PUPP_REPOS/' + orgParamName + "/" + repo_name_param
   repo_branch = branchParam
+
+  all_devs_in_repo = getDevsOfRepo(repo_path)  
+  print all_devs_in_repo 
 
   all_pp_files_in_repo = getPuppetFilesOfRepo(repo_path)
   
